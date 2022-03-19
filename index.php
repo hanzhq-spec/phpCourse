b6d53c31b7f13eaaa61ff313541920fbeae798fb
hanzhq-macAir <hanzhq-macAir>
2022-03-19 15:05:31 +0800

    hanzhq2022319

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<h1>测试</h1>
-<?php infophp()?>
+<?php
+/*!
+ * phpCourse v1.0 (https://github.com/hanzhq-spec/outputjson)
+ * Copyright hanzhq-spec
+ * Licensed under MIT
+ *
+ * Purpose:
+ *   - 测试同步代码到Github hanzhq2022319
+ * 
+/*!*/
+
+phpinfo();
+
+?>
